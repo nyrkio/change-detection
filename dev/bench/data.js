a6bb1288b20d68870790c90198e4564dd84c7284
github-action-benchmark <github@users.noreply.github.com>
2025-04-07 02:02:29 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for f9519e35137a83fba78ff72c4e219ac2bafb8a90

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743991346706,
+  "lastUpdate": 1743991348985,
   "entries": {
     "Rust Benchmark": [
       {
@@ -44589,6 +44589,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00005042019538286072",
             "extra": "mean: 1.645769354680329 msec\nrounds: 609"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "f9519e35137a83fba78ff72c4e219ac2bafb8a90",
+          "message": "Point all legacy tests to nyrkio/change-detection\n\nRemove all upstream contributors from alert-cc-on-comment",
+          "timestamp": "2025-04-07T04:50:16+03:00",
+          "tree_id": "dd252801a2c3f957c8a81516728df531dd2aae97",
+          "url": "https://github.com/nyrkio/change-detection/commit/f9519e35137a83fba78ff72c4e219ac2bafb8a90"
+        },
+        "date": 1743991345548,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "bench.py::test_fib_10",
+            "value": 75007.28397758873,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000010098562659002297",
+            "extra": "mean: 13.332038529735163 usec\nrounds: 38360"
+          },
+          {
+            "name": "bench.py::test_fib_20",
+            "value": 606.2764854871597,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000013091938678009316",
+            "extra": "mean: 1.649412477537328 msec\nrounds: 601"
           }
         ]
       }
