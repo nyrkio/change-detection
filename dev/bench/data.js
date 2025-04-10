cfaf2fa230e69db0bbac7c70b05b9bc72ce500ea
github-action-benchmark <github@users.noreply.github.com>
2025-04-10 21:42:18 +0000

    add Rust Benchmark (cargo) benchmark result for aecf35bbf7c86106fe49e2aa143de52550f655e0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744321336637,
+  "lastUpdate": 1744321338341,
   "entries": {
     "Rust Benchmark": [
       {
@@ -12454,6 +12454,42 @@ window.BENCHMARK_DATA = {
             "name": "bench_fib_20",
             "value": 25203.32,
             "range": "± 317.06",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "aecf35bbf7c86106fe49e2aa143de52550f655e0",
+          "message": "README fixes + a better pull request comment example",
+          "timestamp": "2025-04-11T00:41:06+03:00",
+          "tree_id": "7f11f3d985f2078f74f34b159121336cc2823015",
+          "url": "https://github.com/nyrkio/change-detection/commit/aecf35bbf7c86106fe49e2aa143de52550f655e0"
+        },
+        "date": 1744321330767,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_fib_10",
+            "value": 159.4,
+            "range": "± 2.15",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_fib_20",
+            "value": 21136.56,
+            "range": "± 232.69",
             "unit": "ns/iter"
           }
         ]
